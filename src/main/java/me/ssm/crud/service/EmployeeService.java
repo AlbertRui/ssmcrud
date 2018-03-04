c9eb41b298ac7d8a526f8d2ea91f17d2f0eceb72
AlbertRui <ZR_sxlx@163.com>
2018-03-04 19:29:42 +0800

    add the BootStrap views

---
+++ src/main/java/me/ssm/crud/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package me.ssm.crud.service;
+
+import me.ssm.crud.bean.Employee;
+import me.ssm.crud.dao.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author AlbertRui
+ * @create 2018-03-04 17:18
+ */
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    /**
+     * 查询所有员工数据
+     * @return
+     */
+    public List<Employee> getAll() {
+        return employeeMapper.selectByExampleWithDept(null);
+    }
+}
